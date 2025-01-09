027bb9f6a3fc94235e258d58fdc155d85a152e4e
daniele.gattuso <d.gattuso09@gmail.com>
2025-01-09 17:17:34 +0100

    done 4th excercize

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -1,9 +1,41 @@
+
+
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
 const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+
+let vowels = word.split("")
+
+
+
+function quantitaVocali(lettere) {
+    
+    let vocali = ["a","e","i","o","u"];
+    let numeroVocali = []
+    for(let i=0; i<word.length ; i++){
+
+        
+        for(let j=0; j<vocali.length; j++){
+
+            if(lettere[i] === vocali[j]){
+
+                numeroVocali.push(lettere[i])
+            }
+        }
+    }
+
+
+    return numeroVocali
+    
+}
+
+let anfora = quantitaVocali(vowels)
+console.log(anfora.length ,anfora);
+
+
 
 
 // Invoca la funzione qui e stampa il risultato in console
