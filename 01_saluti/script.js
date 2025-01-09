43670b6389b995019e6f8459155db969c44cfdb4
daniele.gattuso <d.gattuso09@gmail.com>
2025-01-09 15:18:08 +0100

    done 1 excercize

---
+++ 01_saluti/script.js
@@ -0,0 +1,23 @@
+
+
+/* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
+
+const userName = 'Mario';
+
+
+
+
+// Dichiara la funzione qui.
+function hi(nomeU) {
+
+
+    console.log("Ciao" ,nomeU);
+    
+}
+
+
+// Invoca la funzione qui e stampa il risultato in console
+
+let saluto = hi(userName);
+
+//Risultato atteso se si passa 'Mario': // ciao Mario
