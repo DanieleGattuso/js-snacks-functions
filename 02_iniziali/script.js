62018bb7aaf0538a6820fdf02e29c1bc10c4d4c5
daniele.gattuso <d.gattuso09@gmail.com>
2025-01-09 15:54:37 +0100

    done 2nd excercize

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -1,4 +1,4 @@
-/* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
+/* Scrivi una funzione che accetti un array di nomi e restituisca un nuovo  array con le iniziali di ogni parola dell'array fornito */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
@@ -6,8 +6,27 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 // Dichiara la funzione qui.
 
 
+function getInitials(array) {
+    
+    const initials = [];
+    
+    let primaLettera = 0;
+   
+    for (let i = 0; i < array.length; i++) {
+        
+        primaLettera = array[i].charAt(0).toUpperCase();
+       
+        initials.push(primaLettera);
+    }
+    
+   
+    return initials;
+}
+
+
+
 // Invoca la funzione qui e stampa il risultato in console
+const initials = getInitials(names);
 
-
-
-//Risultato atteso: ["A", "L", "M", "A", "G", "A"]
+// Stampiamo il risultato
+console.log(initials); // Output: ["A", "L", "M", "A", "G", "A"]
